433af18f25dccba7d37d812845e5d40a2964a6d7
Dale Newman <dalenewman@gmail.com>
2019-03-18 15:48:47 -0400

    updated nuget script

--- nuget/nuget.bat
+++ nuget/nuget.bat
@@ -1,8 +1,8 @@
 nuget pack Transformalize.Provider.Sqlite.nuspec -OutputDirectory "c:\temp\modules"
 nuget pack Transformalize.Provider.Sqlite.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 
-REM nuget push "c:\temp\modules\Transformalize.Provider.Sqlite.0.4.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.Sqlite.Autofac.0.4.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.Sqlite.0.4.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.Sqlite.Autofac.0.4.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
 
 
 
